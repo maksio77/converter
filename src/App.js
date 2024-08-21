ef3100b82f3c0b74d978d269ba5e01ac4f993e65
Maksym Pavliv <maksiopavliv0@gmail.com>
2024-08-21 12:08:51 +0300

    deploy commit

--- src/App.js
+++ src/App.js
@@ -1,61 +1,62 @@
-import React from 'react';
-import { Block } from './Block';
-import './index.css';
+import React from "react";
+import { Block } from "./Block";
+import "./index.css";
 
 function App() {
-  const[fromCurrency, setFromCurrency] = React.useState('UAH');
-  const[toCurrency, setToCurrency] = React.useState('USD');
-  const[fromPrice, setFromPrice] = React.useState(0);
-  const[toPrice, setToPrice] = React.useState(1);
+  const [fromCurrency, setFromCurrency] = React.useState("UAH");
+  const [toCurrency, setToCurrency] = React.useState("USD");
+  const [fromPrice, setFromPrice] = React.useState(0);
+  const [toPrice, setToPrice] = React.useState(1);
 
   //const[rates, setRates] = React.useState({});
   const ratesRef = React.useRef({});
 
   React.useEffect(() => {
-    fetch('https://www.cbr-xml-daily.ru/latest.js')
-    .then((res) => res.json())
-    .then((json) => {
-      ratesRef.current = json.rates;
-      onChangeToPrice(1);
-    })
-    .catch((err) => {
-      console.warn(err);
-      alert(('Невдалось отримати дані'))
-    });
+    fetch("https://www.cbr-xml-daily.ru/latest.js")
+      .then((res) => res.json())
+      .then((json) => {
+        ratesRef.current = json.rates;
+        onChangeToPrice(1);
+      })
+      .catch((err) => {
+        console.warn(err);
+        alert("Невдалось отримати дані");
+      });
   }, []);
 
   const onChangeFromPrice = (value) => {
     const price = value / ratesRef.current[fromCurrency];
-    const result = (price * ratesRef.current[toCurrency]);
+    const result = price * ratesRef.current[toCurrency];
     setToPrice(result.toFixed(3));
     setFromPrice(value);
-  }
+  };
 
   const onChangeToPrice = (value) => {
-    const result = ((ratesRef.current[fromCurrency]/ratesRef.current[toCurrency]) * value);
+    const result =
+      (ratesRef.current[fromCurrency] / ratesRef.current[toCurrency]) * value;
     setToPrice(value);
     setFromPrice(result.toFixed(3));
-  }
+  };
 
   React.useEffect(() => {
     onChangeToPrice(toPrice);
-  },[toCurrency]);
+  }, [toCurrency]);
 
   React.useEffect(() => {
     onChangeFromPrice(fromPrice);
-  },[fromCurrency]);
+  }, [fromCurrency]);
 
   return (
     <div className="App">
-      <Block 
-        value={fromPrice} 
-        currency={fromCurrency} 
-        onChangeCurrency={setFromCurrency} 
+      <Block
+        value={fromPrice}
+        currency={fromCurrency}
+        onChangeCurrency={setFromCurrency}
         onChangeValue={onChangeFromPrice}
       />
-      <Block 
-        value={toPrice} 
-        currency={toCurrency} 
+      <Block
+        value={toPrice}
+        currency={toCurrency}
         onChangeCurrency={setToCurrency}
         onChangeValue={onChangeToPrice}
       />
